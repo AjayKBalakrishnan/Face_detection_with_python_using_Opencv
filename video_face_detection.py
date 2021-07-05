9679a62b0dbd24cec7683595f5d88eac4188877c
AjayKBalakrishnan <86957162+AjayKBalakrishnan@users.noreply.github.com>
2021-07-06 01:22:47 +0530

    Add files via upload

---
+++ video_face_detection.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+path = r'D:\programing\Visual Studio\testimg&video\testvideo1.mp4'
+cap = cv.VideoCapture(path)
+
+while True:
+    _,img = cap.read()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    faces  = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    for (x,y,w,h) in faces:
+        cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+        
+    cv.imshow('dectected Faces',img)
+
+    if cv.waitKey(30) & 0xFF == ord('d'):
+        break
+
+cap.release()
